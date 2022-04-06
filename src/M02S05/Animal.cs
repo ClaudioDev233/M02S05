ac70960d81e28830d23b95c020abaccb890bdcbb
Claudio <claudio.junior233@gmail.com>
2022-04-06 14:58:48 -0300

    Inicio Zoologico

---
+++ src/M02S05/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M02S05
+{
+    public abstract class Animal
+    {
+        public string Nome { get; private set; }
+        public int Idade { get; private set; }
+    }
+}
---
+++ src/M02S05/Cachorro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M02S05
+{
+    public class Cachorro : Animal, ICorrer
+    {
+        public void Correr()
+        {
+            Console.WriteLine("Cachorrin Correndo");
+        }
+    }
+}
